aa69f0ac6afec67a9a15dbcff24178f76d3c50d3
Sam Groves <samuelhgroves@gmail.com>
2022-12-09 10:57:34 -0500

    Added erase, shade, and highlight buttons. Improved UI and added breakpoint for desktop

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ const container = document.querySelector('.container');
 let gridSize = 16;
 let root = document.querySelector(':root');
 root.style.setProperty('--gridSize', gridSize);
-
 function resetGrid() {
     while (container.firstChild) {
         container.removeChild(container.firstChild)
     }
 }
-
 function makeGrid(gridSize) {
     resetGrid();
     for (let i = 0; i < gridSize * gridSize ; i++) {
         box = document.createElement('box');
         box.className = 'box';
-        
+        box.style.backgroundColor = "white";
+        box.style.filter = "brightness(1)";
         container.appendChild(box);
     }
 }
@@ -26,30 +25,127 @@ window.mouseDown = false;
 document.onmousedown = function() {
     window.mouseDown = true;
 }
+
 document.onmouseup = function() {
     window.mouseDown = false;
 }
-document.addEventListener("mouseover", function(e){
+
+document.addEventListener('mouseover', handler, false);
+document.addEventListener('mousedown', handler, false);
+function handler(e) {
     const target = e.target.closest(".box"); 
     if (window.mouseDown == true) {
         if(target){
-            target.style.backgroundColor = color;
-          }
-    }
-  });
+            let style = getComputedStyle(target);
+                let brightness = style.filter;
+                console.log(brightness);
+            if (highlight.classList.contains('active')) {
+                if (brightness == 'brightness(0)') {
+                    target.style.filter = 'brightness(0.1)'
+                } else if (brightness == 'brightness(0.1)'){
+                    target.style.filter = 'brightness(0.2)'
+                } else if (brightness == 'brightness(0.2)'){
+                    target.style.filter = 'brightness(0.3)'
+                } else if (brightness == 'brightness(0.3)'){
+                    target.style.filter = 'brightness(0.4)'
+                } else if (brightness == 'brightness(0.4)'){
+                    target.style.filter = 'brightness(0.5)'
+                } else if (brightness == 'brightness(0.5)'){
+                    target.style.filter = 'brightness(0.6)'
+                } else if (brightness == 'brightness(0.6)'){
+                    target.style.filter = 'brightness(0.7)'
+                } else if (brightness == 'brightness(0.7)'){
+                    target.style.filter = 'brightness(0.8)'
+                } else if (brightness == 'brightness(0.8)'){
+                    target.style.filter = 'brightness(0.9)'
+                } else if (brightness == 'brightness(0.9)'){
+                    target.style.filter = 'brightness(1)'
+                } else if (brightness == 'brightness(1)'){
+                    target.style.filter = 'brightness(1.1)'
+                } else if (brightness == 'brightness(1.1)'){
+                    target.style.filter = 'brightness(1.2)'
+                } else if (brightness == 'brightness(1.2)'){
+                    target.style.filter = 'brightness(1.3)'
+                } else if (brightness == 'brightness(1.3)'){
+                    target.style.filter = 'brightness(1.4)'
+                } else if (brightness == 'brightness(1.4)'){
+                    target.style.filter = 'brightness(1.5)'
+                } else if (brightness == 'brightness(1.5)'){
+                    target.style.filter = 'brightness(1.6)'
+                } else if (brightness == 'brightness(1.6)'){
+                    target.style.filter = 'brightness(1.7)'
+                } else if (brightness == 'brightness(1.7)'){
+                    target.style.filter = 'brightness(1.8)'
+                } else if (brightness == 'brightness(1.8)'){
+                    target.style.filter = 'brightness(1.9)'
+                } else if (brightness == 'brightness(1.9)'){
+                    target.style.filter = 'brightness(2)'
+                } else if (brightness == 'brightness(2)'){
+                    target.style.filter = 'brightness(2.5)'
+                } else if (brightness == 'brightness(2.5)'){
+                    target.style.filter = 'brightness(3.5)'
+                } else if (brightness == 'brightness(3.5)'){
+                    target.style.filter = 'brightness(5)';
+                }
+            } else if (shade.classList.contains('active')) {
+                if (brightness == 'brightness(5)'){
+                    target.style.filter = 'brightness(3.5)'
+                } else if (brightness == 'brightness(3.5)'){
+                    target.style.filter = 'brightness(2.5)'
+                } else if (brightness == 'brightness(2.5)'){
+                    target.style.filter = 'brightness(2)'
+                } else if (brightness == 'brightness(2)'){
+                    target.style.filter = 'brightness(1.9)'
+                } else if (brightness == 'brightness(1.9)'){
+                    target.style.filter = 'brightness(1.8)'
+                } else if (brightness == 'brightness(1.8)'){
+                    target.style.filter = 'brightness(1.7)'
+                } else if (brightness == 'brightness(1.7)'){
+                    target.style.filter = 'brightness(1.6)'
+                } else if (brightness == 'brightness(1.6)'){
+                    target.style.filter = 'brightness(1.5)'
+                } else if (brightness == 'brightness(1.5)'){
+                    target.style.filter = 'brightness(1.4)'
+                } else if (brightness == 'brightness(1.4)'){
+                    target.style.filter = 'brightness(1.3)'
+                } else if (brightness == 'brightness(1.3)'){
+                    target.style.filter = 'brightness(1.2)'
+                } else if (brightness == 'brightness(1.2)'){
+                    target.style.filter = 'brightness(1.1)'
+                } else if (brightness == 'brightness(1.1)'){
+                    target.style.filter = 'brightness(1)'
+                } else if (brightness == 'brightness(1)'){
+                    target.style.filter = 'brightness(0.9)'
+                } else if (brightness == 'brightness(0.9)'){
+                    target.style.filter = 'brightness(0.8)'
+                } else if (brightness == 'brightness(0.8)'){
+                    target.style.filter = 'brightness(0.7)'
+                } else if (brightness == 'brightness(0.7)'){
+                    target.style.filter = 'brightness(0.6)'
+                } else if (brightness == 'brightness(0.6)'){
+                    target.style.filter = 'brightness(0.5)'
+                } else if (brightness == 'brightness(0.5)'){
+                    target.style.filter = 'brightness(0.4)'
+                } else if (brightness == 'brightness(0.4)'){
+                    target.style.filter = 'brightness(0.3)'
+                } else if (brightness == 'brightness(0.3)'){
+                    target.style.filter = 'brightness(0.2)'
+                } else if (brightness == 'brightness(0.2)'){
+                    target.style.filter = 'brightness(0.1)'
+                } else if (brightness == 'brightness(0.1)'){
+                    target.style.filter = 'brightness(0)'
+                }
+            }
+                else {
+                target.style.backgroundColor = color;
+                target.style.filter = 'brightness(1)';
+            }}
+  }};
 
-document.addEventListener("mousedown", function(e){
-    const target = e.target.closest(".box");
-        if(target){
-            target.style.backgroundColor = color;
-          }
-  });
-
-//Slider
+//Grid Size Slider
 let slider = document.getElementById('myRange');
 let output = document.getElementById('sliderValue');
 output.innerText = slider.value;
-
 slider.oninput = function() {
     output.innerHTML = this.value;
     gridSize = this.value;
@@ -57,9 +153,66 @@ slider.oninput = function() {
     root.style.setProperty('--gridSize', this.value);
   }
 
+let color = '#30D5C8';
 
-let colorPicker = document.getElementById('colorpicker');
-let color = '#000000';
-colorPicker. oninput = function () {
-color = this.value;
-  }
+//Color Picker
+const colorPicker = document.getElementById('colorpicker');
+let pickColor = '#0A0A0A';
+colorPicker.oninput = function () {
+    pickColor = this.value;
+    if (colorBtn.classList.contains('active')) {
+        color = pickColor;
+    }
+  };
+
+//Color Button
+const colorBtn = document.getElementById('color');
+function colorMode() {
+    color = pickColor;
+    colorBtn.className = 'active';
+    eraser.className = '';
+    highlight.className = '';
+    shade.className = '';
+
+}
+colorBtn.addEventListener('click', colorMode);
+
+
+//Eraser
+const eraser = document.getElementById('eraser');
+eraser.addEventListener('click', erase);
+function erase() {
+    color = '#ffffff';
+    eraser.className = 'active';
+    colorBtn.className = '';
+    highlight.className = '';
+    shade.className = '';
+}
+
+//Highlight
+const highlight = document.getElementById('highlight');
+highlight.addEventListener('click', lighten);
+function lighten() {
+    highlight.className = 'active';
+    eraser.className = '';
+    colorBtn.className = '';
+    shade.className = '';
+}
+
+//Shade
+const shade = document.getElementById('shade');
+shade.addEventListener('click', darken);
+function darken() {
+    shade.className = 'active';
+    highlight.className = '';
+    eraser.className = '';
+    colorBtn.className = '';
+}
+
+//Reset
+const reset = document.getElementById('reset');
+reset.addEventListener('click', newGrid);
+function newGrid() {
+    resetGrid();
+    makeGrid(gridSize);
+}
